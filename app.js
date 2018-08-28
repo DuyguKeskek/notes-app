10db6dc907d0f94212cd581ae2a2979b1a41aa89
Duygu Keşkek <duygu.keskek@Barcins-Mac-mini.local>
2018-08-28 11:58:35 +0300

    basic setup

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('Starting app.js');
+
+const fs = require('fs'); //call methods containing methods, require nodejs modules
+const _ = require('lodash'); //require 3rd party modules
+
+const notes = require('./notes.js'); //reqire own files
