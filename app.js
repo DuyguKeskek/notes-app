c001838e09cd8c86a78dc9725b3f44f43e3101af
Duygu Keşkek <duygu.keskek@Barcins-Mac-mini.local>
2018-08-28 14:12:36 +0300

    created command line arguments

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const fs = require('fs'); //call methods containing methods, require nodejs modu
 const _ = require('lodash'); //require 3rd party modules
 
 const notes = require('./notes.js'); //reqire own files
+
+var command = process.argv[2]; //enables commands
+console.log('Command:', command);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading note');
+} else if (command === 'remove') {
+    console.log('Removing note');
+} else {
+    console.log('Command not found');
+}
